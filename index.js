34eb297ab070f017c74f8dcde72716dd0297ab1a
cmgPu <developers_cro@convergent-usa.com>
2019-10-15 15:59:21 +0200

    1 file modified, 1 added, 2 deleted

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import About from './components/About';
+import General from './components/General';
 import Home from './components/Home';
-import Topics from './components/Topics';
 
 const BasicExample = () => (
   <Router>
     <div>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><Link to="/about">General</Link></li>
       </ul>
 
       <hr />
 
       <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <Route path="/general" component={General} />
     </div>
   </Router>
 );
